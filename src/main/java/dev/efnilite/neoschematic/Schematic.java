e830d2371369a2d7eeb9ec19b474dd48168a5a1d
Efnilite <35348263+Efnilite@users.noreply.github.com>
2024-05-12 22:51:37 +0200

    Re-add migrator code

--- src/main/java/dev/efnilite/neoschematic/Schematic.java
+++ src/main/java/dev/efnilite/neoschematic/Schematic.java
@@ -12,11 +12,9 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 import java.util.concurrent.CompletableFuture;
+import java.util.function.Function;
 
 /**
  * Creates a new {@link Schematic} instance. This should only be used for custom {@link FileType} implementations.
@@ -30,6 +28,12 @@ import java.util.concurrent.CompletableFuture;
  */
 public record Schematic(int dataVersion, String minecraftVersion, Vector dimensions,
                         List<BlockData> palette, List<Short> blocks) {
+
+    private static final Map<String, List<Function<String, String>>> migrators = new TreeMap<>();
+
+    static {
+        addMigrator("1.19.4", (data) -> data.replace("minecraft:grass", "minecraft:short_grass"));
+    }
 
     /**
      * Synchronously gets and stores all blocks between the positions in a new {@link Schematic} instance.
@@ -207,6 +211,25 @@ public record Schematic(int dataVersion, String minecraftVersion, Vector dimensi
         var palette = new ArrayList<>(paletteMap.keySet());
 
         return new BlocksData(dimensions, palette, blocks);
+    }
+
+    /**
+     * Adds a migrator function for the specified version.
+     * If version incompatibilities exist, the migrator will be called to update the {@link BlockData}.
+     * This only updates to newer versions.
+     *
+     * @param version The version string. Example: "1.19.4".
+     * @param migrator The migrator function.
+     */
+    public static void addMigrator(String version, Function<String, String> migrator) {
+        migrators.computeIfAbsent(version, k -> new ArrayList<>()).add(migrator);
+    }
+
+    /**
+     * @return the migrators map. Sorted by version.
+     */
+    public static Map<String, List<Function<String, String>>> getMigrators() {
+        return migrators;
     }
 
     /**
