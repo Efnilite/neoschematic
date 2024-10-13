763572fdb864a32950d659098e2e4bb3bc5bc19f
Efnilite <35348263+Efnilite@users.noreply.github.com>
2024-10-13 14:11:11 +0200

    update format

--- src/test/java/dev/efnilite/neoschematic/test/JsonSchematicTest.java
+++ src/test/java/dev/efnilite/neoschematic/test/JsonSchematicTest.java
@@ -1,36 +1,36 @@
 package dev.efnilite.neoschematic.test;
 
 import dev.efnilite.neoschematic.JsonSchematic;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 import java.util.stream.IntStream;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.assertEquals;
 
-public class JsonSchematicTest {
+public class JsonSchematicTest extends TestRunner {
 
-    private final JsonSchematic jsonSchematic = new JsonSchematic();
+    private static final JsonSchematic JSON_SCHEMATIC = new JsonSchematic();
 
     @Test
     public void testToChar() {
-        var chars = IntStream.range(0, 100).mapToObj(i -> jsonSchematic.getChar((short) i)).toList();
+        var chars = IntStream.range(0, 100).mapToObj(i -> JSON_SCHEMATIC.getChar((short) i)).toList();
         var str = String.join("", chars);
 
-        assertEquals(str.charAt(0), '#');
-        assertEquals(str.charAt(1), '$');
-        assertEquals(str.charAt(91), '~');
-        assertEquals(str.charAt(92), ' ');
-        assertEquals(str.charAt(93), '¡'); // start of non-ascii
+        assertEquals('#', str.charAt(0));
+        assertEquals('$', str.charAt(1));
+        assertEquals('~', str.charAt(91));
+        assertEquals(' ', str.charAt(92));
+        assertEquals('¡', str.charAt(93)); // start of non-ascii
     }
 
     @Test
     public void testFromChar() {
-        var idxs = "#$~ ¡".chars().map(c -> jsonSchematic.fromChar((char) c)).boxed().toList();
+        var idxs = "#$~ ¡".chars().map(c -> JSON_SCHEMATIC.fromChar((char) c)).boxed().toList();
 
-        assertEquals(idxs.get(0), 0);
-        assertEquals(idxs.get(1), 1);
-        assertEquals(idxs.get(2), 91);
-        assertEquals(idxs.get(3), 92);
-        assertEquals(idxs.get(4), 93);
+        assertEquals(0, (int) idxs.get(0));
+        assertEquals(1, (int) idxs.get(1));
+        assertEquals(91, (int) idxs.get(2));
+        assertEquals(92, (int) idxs.get(3));
+        assertEquals(93, (int) idxs.get(4));
     }
 }
